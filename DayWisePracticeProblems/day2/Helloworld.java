6c5103d6606c034b0ab2daba0299236d1f300e03
sumithanvat <skhanvat@gmail.com>
2023-02-09 13:17:49 +0530

    add : Day2 practice problem added

---
+++ DayWisePracticeProblems/day2/Helloworld.java
@@ -0,0 +1,10 @@
+class HelloWorld {
+
+	public static void main(String[] args) { while(true) { 
+			System.out.println("Hello World!!!"); try {
+				Thread.sleep(5000);
+			} catch(Exception e){
+			}
+		}
+	}
+}
